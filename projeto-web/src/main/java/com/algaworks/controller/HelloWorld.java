b3c6bd700f4786b47712cfb9b252541522d3fd3a
Michael Vambaster <45100957+mvambaster@users.noreply.github.com>
2018-12-06 14:55:57 -0200

    Create HelloWorld.java

---
+++ projeto-web/src/main/java/com/algaworks/controller/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+  public static void main(String [] args){
+     System.out.println("Teste funcionou da classe HelloWorld, do projeto Aula-comecando-com-maven... copy Michael Vambaster");
+  }
+}
